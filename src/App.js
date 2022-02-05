544c579e07071fa417cc2adbe0107052f6f2586f
dshan <abcww2@naver.com>
2022-02-05 13:45:04 +0900

    part1-009

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./App.css"
-import LifecycleEx from './R008_LifecycleEx';
+import Es6 from './R009_Es6';
 
 
 
@@ -9,9 +9,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <LifecycleEx
-        prop_value = 'FromApp.js'
-      />
+      <Es6/>
     </div>
   );
 }
