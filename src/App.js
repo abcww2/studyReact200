091e75d6651d1cf502ddcff3a607245d31673667
dshan <abcww2@naver.com>
2022-02-14 01:05:46 +0900

    part1-013

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import "./App.css"
-import ClassPrototype from './R012_class&Prototype'
-
+import ArrowFunction from './R013_ArrowFunction'
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ClassPrototype/>
+      <ArrowFunction/>
     </div>
   );
 }
